a27ec3e2008dc960e1a5c95425d1826649018e7b
narsimha-dev <snlshvsnj@gmial.com>
2020-02-24 17:46:30 +0530

    0.1: get user details

--- src/App.js
+++ src/App.js
@@ -1,35 +1,10 @@
 import React from 'react';
-import Button from '@material-ui/core/Button';
-import './App.css'; 
-import SidebarMenu from './component/sidebar/SidebarMenu';
-import Users from './component/Users';
+import MainRouter from './component/MainRouter';
 
-class App extends React.Component {
-  constructor(props){
-    super(props);
-    this.state={
-      isLoad:false
-    }
-  }
-  sidebar=(e)=>{
-    e.preventDefault();
-    this.setState({isLoad:!this.state.isLoad})
-  }
-  render(){
-    if(this.state.isLoad){
-      // return <SidebarMenu/>
-      return <Users/>
-    }
-  return (
-    <div className="App">
-      <header className="App-header">    
-      <Button onClick={e=>this.sidebar(e)}variant="contained" color="primary">
-      Click load sidbar
-    </Button>
-      </header>
+export const App=()=>{
+  return(
+    <div>
+      <MainRouter/>
     </div>
-  );
+  )
   }
-}
-
-export default App;
